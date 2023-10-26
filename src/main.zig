5d6f60d2334784250e5b288fc2835aa2941c497c
Chase Bradley <bradleycha@proton.me>
2023-10-26 12:47:54 -0400

    Heap allocator

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,37 @@
 const std         = @import("std");
+const builtin     = @import("builtin");
 const window      = @import("window");
 const graphics    = @import("graphics");
 const resources   = @import("resources");
 
-pub fn main() void {
-   std.io.getStdOut().writer().print("Hello, world!\n", .{}) catch {};
+const MainError = error {
+   OutOfMemory,
+};
+
+pub fn main() MainError!void {
+   var heap = chooseHeapSettings();
+   defer _ = heap.deinit();
+
+   const allocator = heap.allocator();
+
    return;
+}
+
+fn chooseHeapSettings() std.heap.GeneralPurposeAllocator(.{}) {
+   const backing_allocator = chooseBackingAllocator();
+
+   return .{.backing_allocator = backing_allocator};
+}
+
+fn chooseBackingAllocator() std.mem.Allocator {
+   if (builtin.link_libc == false) {
+      return std.heap.page_allocator;
+   }
+
+   if (builtin.mode == .Debug) {
+      return std.heap.page_allocator;
+   }
+
+   return std.heap.raw_c_allocator;
 }
 
