665c51c9948655ce6c7bec637ee0e057149f8f30
Chase Bradley <bradleycha@proton.me>
2023-10-28 13:12:50 -0400

    Physical device selection

--- src/graphics/vulkan/physical_device.zig
+++ src/graphics/vulkan/physical_device.zig
@@ -4,6 +4,15 @@ const c     = @import("cimports");
 
 pub const QueueFamilyIndices = struct {
    graphics : u32,
+   transfer : u32,
+
+   pub const INFO = struct {
+      pub const Count = 2;
+      pub const Index = struct {
+         pub const Graphics   = 0;
+         pub const Transfer   = 1;
+      };
+   };
 };
 
 pub const PhysicalDevice = struct {
@@ -58,10 +67,36 @@ pub const PhysicalDevice = struct {
    }
 
    fn _parsePhysicalDevice(allocator : std.mem.Allocator, vk_physical_device : c.VkPhysicalDevice, enabled_extensions : [] const [*:0] const u8) SelectError!?@This() {
-      _ = allocator;
-      _ = vk_physical_device;
-      _ = enabled_extensions;
-      unreachable;
+      var vk_physical_device_properties : c.VkPhysicalDeviceProperties = undefined;
+      c.vkGetPhysicalDeviceProperties(vk_physical_device, &vk_physical_device_properties);
+
+      var vk_physical_device_features : c.VkPhysicalDeviceFeatures = undefined;
+      c.vkGetPhysicalDeviceFeatures(vk_physical_device, &vk_physical_device_features);
+
+      var vk_physical_device_memory_properties : c.VkPhysicalDeviceMemoryProperties = undefined;
+      c.vkGetPhysicalDeviceMemoryProperties(vk_physical_device, &vk_physical_device_memory_properties);
+
+      const vk_physical_device_name = &vk_physical_device_properties.deviceName;
+
+      std.log.info("attempting to enumerate vulkan physical device \"{s}\"", .{vk_physical_device_name});
+
+      if (try _checkExtensionsPresent(allocator, vk_physical_device, enabled_extensions) == false) {
+         std.log.info("vulkan physical device \"{s}\" does not support enabled extensions, choosing new device", .{vk_physical_device_name});
+         return null;
+      }
+
+      const queue_family_indices = try _selectQueueFamilyIndices(allocator, vk_physical_device) orelse {
+         std.log.info("vulkan physical device \"{s}\" does not support required queue families, choosing new device" , .{vk_physical_device_name});
+         return null;
+      };
+
+      return @This(){
+         .vk_physical_device                    = vk_physical_device,
+         .vk_physical_device_properties         = vk_physical_device_properties,
+         .vk_physical_device_features           = vk_physical_device_features,
+         .vk_physical_device_memory_properties  = vk_physical_device_memory_properties,
+         .queue_family_indices                  = queue_family_indices,
+      };
    }
 
    fn _assignScore(self : * const @This()) u32 {
@@ -104,5 +139,118 @@ fn _enumeratePhysicalDevices(allocator : std.mem.Allocator, vk_instance : c.VkIn
    }
 
    return vk_physical_devices;
+}
+
+fn _checkExtensionsPresent(allocator : std.mem.Allocator, vk_physical_device : c.VkPhysicalDevice, enabled_extensions : [] const [*:0] const u8) PhysicalDevice.SelectError!bool {
+   var vk_result : c.VkResult = undefined;
+
+   var vk_extensions_available_count : u32 = undefined;
+   vk_result = c.vkEnumerateDeviceExtensionProperties(vk_physical_device, null, &vk_extensions_available_count, null);
+   switch (vk_result) {
+      c.VK_SUCCESS                     => {},
+      c.VK_INCOMPLETE                  => {},
+      c.VK_ERROR_OUT_OF_HOST_MEMORY    => return error.OutOfMemory,
+      c.VK_ERROR_OUT_OF_DEVICE_MEMORY  => return error.OutOfMemory,
+      c.VK_ERROR_LAYER_NOT_PRESENT     => unreachable,
+      else                             => unreachable,
+   }
+
+   var vk_extensions_available = try allocator.alloc(c.VkExtensionProperties, @as(usize, vk_extensions_available_count));
+   defer allocator.free(vk_extensions_available);
+   vk_result = c.vkEnumerateDeviceExtensionProperties(vk_physical_device, null, &vk_extensions_available_count, vk_extensions_available.ptr);
+   switch (vk_result) {
+      c.VK_SUCCESS                     => {},
+      c.VK_INCOMPLETE                  => {},
+      c.VK_ERROR_OUT_OF_HOST_MEMORY    => return error.OutOfMemory,
+      c.VK_ERROR_OUT_OF_DEVICE_MEMORY  => return error.OutOfMemory,
+      c.VK_ERROR_LAYER_NOT_PRESENT     => unreachable,
+      else                             => unreachable,
+   }
+
+   var everything_found = true;
+   for (enabled_extensions) |enabled_extension| {
+      var found = false;
+      for (vk_extensions_available) |vk_extension_available| {
+         if (c.strcmp(&vk_extension_available.extensionName, enabled_extension) == 0) {
+            found = true;
+            break;
+         }
+      }
+      if (found == false) {
+         everything_found = false;
+         std.log.info("missing required vulkan device extension \"{s}\"", .{enabled_extension});
+      }
+   }
+
+   return everything_found;
+}
+
+fn _selectQueueFamilyIndices(allocator : std.mem.Allocator, vk_physical_device : c.VkPhysicalDevice) PhysicalDevice.SelectError!?QueueFamilyIndices {
+   var vk_physical_device_queue_families_count : u32 = undefined;
+   c.vkGetPhysicalDeviceQueueFamilyProperties(vk_physical_device, &vk_physical_device_queue_families_count, null);
+
+   var vk_physical_device_queue_families = try allocator.alloc(c.VkQueueFamilyProperties, @as(usize, vk_physical_device_queue_families_count));
+   defer allocator.free(vk_physical_device_queue_families);
+   c.vkGetPhysicalDeviceQueueFamilyProperties(vk_physical_device, &vk_physical_device_queue_families_count, vk_physical_device_queue_families.ptr);
+
+   var queue_family_indices_found = [1] ? u32 {null} ** QueueFamilyIndices.INFO.Count;
+
+   for (vk_physical_device_queue_families, 0..vk_physical_device_queue_families_count) |vk_physical_device_queue_family, vk_physical_device_queue_family_index_raw| {
+      const vk_physical_device_queue_family_index : u32 = @intCast(vk_physical_device_queue_family_index_raw);
+
+      var queue_families_found = [1] bool {false} ** QueueFamilyIndices.INFO.Count;
+
+      if (vk_physical_device_queue_family.queueFlags & c.VK_QUEUE_GRAPHICS_BIT != 0) {
+         queue_families_found[QueueFamilyIndices.INFO.Index.Graphics] = true;
+      }
+      if (vk_physical_device_queue_family.queueFlags & c.VK_QUEUE_TRANSFER_BIT != 0) {
+         queue_families_found[QueueFamilyIndices.INFO.Index.Transfer] = true;
+      }
+
+      _assignUniqueQueueFamilyIndices(vk_physical_device_queue_family_index, &queue_families_found, &queue_family_indices_found);
+   }
+
+   const queue_family_indices = QueueFamilyIndices{
+      .graphics   = queue_family_indices_found[QueueFamilyIndices.INFO.Index.Graphics] orelse return null,
+      .transfer   = queue_family_indices_found[QueueFamilyIndices.INFO.Index.Transfer] orelse return null,
+   };
+
+   return queue_family_indices;
+}
+
+fn _assignUniqueQueueFamilyIndices(vk_physical_device_queue_family_index : u32, queue_families_found : * const [QueueFamilyIndices.INFO.Count] bool, queue_family_indices_found : * [QueueFamilyIndices.INFO.Count] ? u32) void {
+   for (queue_families_found, queue_family_indices_found) |queue_family_found, *queue_family_index| {
+      if (queue_family_found == false) {
+         continue;
+      }
+
+      if (queue_family_index.* == null) {
+         queue_family_index.* = vk_physical_device_queue_family_index;
+         continue;
+      }
+
+      var queue_family_index_already_in_use = false;
+      for (queue_family_indices_found) |*queue_family_index_existing| {
+         if (queue_family_index_existing == queue_family_index) {
+            continue;
+         }
+
+         if (queue_family_index_existing.* == null) {
+            continue;
+         }
+
+         if (queue_family_index_existing.* orelse unreachable == queue_family_index.* orelse unreachable) {
+            queue_family_index_already_in_use = true;
+            break;
+         }
+      }
+
+      if (queue_family_index_already_in_use == true) {
+         queue_family_index.* = vk_physical_device_queue_family_index;
+         continue;
+      }
+   }
+
+   return;
 }
 
