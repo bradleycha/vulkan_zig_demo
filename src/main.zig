045e4e5c0432ac585bc42efc8214366d743e8a79
Chase Bradley <bradleycha@proton.me>
2023-11-04 02:42:52 -0400

    Add delta timer and dynamic window title with current FPS

--- src/main.zig
+++ src/main.zig
@@ -7,18 +7,25 @@ const resources   = @import("resources");
 
 const MainError = error {
    OutOfMemory,
+   DeltaTimerStartFailure,
+   WindowTitleTimerStartFailure,
    CompositorConnectError,
    WindowCreateError,
    RendererCreateError,
 };
 
-const PROGRAM_NAME = "Learn Graphics Programming with Zig!";
+const PROGRAM_NAME                     = "Learn Graphics Programming with Zig!";
+const WINDOW_TITLE_UPDATE_TIME_SECONDS = 1.0;
 
 pub fn main() MainError!void {
    var heap = chooseHeapSettings();
    defer _ = heap.deinit();
 
    const allocator = heap.allocator();
+
+   std.log.info("starting timers", .{});
+   var timer_delta         = std.time.Timer.start() catch return error.DeltaTimerStartFailure;
+   var timer_window_title  = std.time.Timer.start() catch return error.DeltaTimerStartFailure;
 
    std.log.info("connecting to desktop compostior", .{});
    var compositor = present.Compositor.connect(allocator) catch return error.CompositorConnectError;
@@ -48,6 +55,26 @@ pub fn main() MainError!void {
 
    std.log.info("initialization complete, entering main loop", .{});
    while (window.shouldClose() == false) {
+      const time_delta        = @as(f64, @floatFromInt(timer_delta.lap())) / 1000000000.0;
+      const time_window_title = @as(f64, @floatFromInt(timer_window_title.read())) / 1000000000.0;
+
+      if (time_window_title > WINDOW_TITLE_UPDATE_TIME_SECONDS) {
+         timer_window_title.reset();
+
+         const fps = 1.0 / time_delta;
+
+         // "In this situation, programmers usually apply..."computer science".
+         // And by..."computer science", I mean picking what seems like a
+         // reasonable buffer length and then doubling it for good measure"
+         //    - Dave Plumber 2021
+         const FORMAT_BUFFER_LENGTH = PROGRAM_NAME.len * 2;
+
+         var format_buffer : [FORMAT_BUFFER_LENGTH:0] u8 = undefined;
+         const title = std.fmt.bufPrintZ(&format_buffer, PROGRAM_NAME ++ " - {d:0.0} fps", .{fps}) catch PROGRAM_NAME;
+
+         window.setTitle(title);
+      }
+
       window.pollEvents() catch |err| {
          std.log.warn("failed to poll window events: {}", .{err});
       };
