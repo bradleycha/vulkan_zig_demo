d593808240e3249625a615778334981018290830
Chase Bradley <bradleycha@proton.me>
2023-11-09 10:10:11 -0500

    Implement memory leak detection and basic helper functions for heap

--- src/structures/heap.zig
+++ src/structures/heap.zig
@@ -21,15 +21,14 @@ pub fn Heap(comptime memory_precision : type) type {
          OutOfMemory,
       };
 
-      const AllocationNode = struct {
-         block : Allocation,
-         next  : usize,
-      };
-
       pub fn deinit(self : @This(), allocator : std.mem.Allocator) void {
-         _ = self;
-         _ = allocator;
-         unreachable;
+         if (std.debug.runtime_safety == true) {
+            self._checkMemoryLeaks();
+         }
+
+         var self_mut = self;
+         self_mut.nodes.deinit(allocator);
+         return;
       }
 
       pub fn allocate(self : * @This(), allocator : std.mem.Allocator, allocate_info : * const AllocateInfo) AllocateError!Allocation {
@@ -45,8 +44,101 @@ pub fn Heap(comptime memory_precision : type) type {
          _ = allocation;
          unreachable;
       }
+
+      const AllocationNode = struct {
+         block : Allocation,
+         next  : usize,
+      };
+
+      const NULL_ALLOCATION_NODE_INDEX = std.math.maxInt(usize);
+      const NULL_ALLOCATION_OFFSET     = std.math.maxInt(memory_precision);
+
+      fn _allocationNodeIsNull(node : * const AllocationNode) bool {
+         return node.block.offset == NULL_ALLOCATION_OFFSET;
+      }
+
+      fn _allocationNodeSetNull(node : * AllocationNode) void {
+         node.block.offset = NULL_ALLOCATION_OFFSET;
+         return;
+      }
+
+      fn _checkAllocationNodeIndex(self : * const @This(), index : usize) bool {
+         return index < self.nodes.items.len;
+      }
+
+      fn _getNodeOptional(self : * const @This(), index : usize) ? * const AllocationNode {
+         if (std.debug.runtime_safety == true and self._checkAllocationNodeIndex(index) == false) {
+            @panic("attempted to access invalid allocation node index");
+         }
+
+         const node = &self.nodes.items[index];
+
+         if (_allocationNodeIsNull(node) == true) {
+            return null;
+         }
+
+         return node;
+      }
+
+      fn _getNodeOptionalMut(self : * @This(), index : usize) ? * AllocationNode {
+         if (std.debug.runtime_safety == true and self._checkAllocationNodeIndex(index) == false) {
+            @panic("attempted to access invalid allocation node index");
+         }
+
+         const node = &self.nodes.items[index];
+
+         if (_allocationNodeIsNull(node) == true) {
+            return null;
+         }
+
+         return node;
+      }
+
+      fn _getNode(self : * const @This(), index : usize) * const AllocationNode {
+         if (std.debug.runtime_safety == true and self._checkAllocationNodeIndex(index) == false) {
+            @panic("attempted to access invalid allocation node index");
+         }
+
+         const node = &self.nodes.items[index];
+
+         if (std.debug.runtime_safety == true and _allocationNodeIsNull(node) == true) {
+            @panic("attempted to access null allocation node");
+         }
+
+         return node;
+      }
+
+      fn _getNodeMut(self : * @This(), index : usize) * AllocationNode {
+         if (std.debug.runtime_safety == true and self._checkAllocationNodeIndex(index) == false) {
+            @panic("attempted to access invalid allocation node index");
+         }
+
+         const node = &self.nodes.items[index];
+
+         if (std.debug.runtime_safety == true and _allocationNodeIsNull(node) == true) {
+            @panic("attempted to access null allocation node");
+         }
+
+         return node;
+      }
+
+      fn _checkMemoryLeaks(self : * const @This()) void {
+         var node_index_curr = self.head;
+         while (node_index_curr != NULL_ALLOCATION_NODE_INDEX) {
+            const node  = self._getNode(node_index_curr);
+            const block = &node.block;
+
+            std.log.err("heap memory leaked at {x} - {x}", .{block.offset, block.offset + block.bytes});
+
+            node_index_curr = node.next;
+         }
+
+         if (self.head != NULL_ALLOCATION_NODE_INDEX) {
+            @panic("heap memory leaked");
+         }
+
+         return;
+      }
    };
 }
-
-const NULL_ALLOCATION_NODE_INDEX = std.math.maxInt(usize);
 
