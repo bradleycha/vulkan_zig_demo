013aa4d6ed64421889047588c7a319bba22b3e42
Chase Bradley <bradleycha@proton.me>
2023-11-12 01:50:05 -0500

    Change clear color to black

--- src/main.zig
+++ src/main.zig
@@ -47,9 +47,9 @@ pub fn main() MainError!void {
       .shader_vertex    = resources.shaders.VERTEX,
       .shader_fragment  = resources.shaders.FRAGMENT,
       .clear_color      = .{.color = .{.channels = .{
-         .r = 1.0,
-         .g = 1.0,
-         .b = 1.0,
+         .r = 0.0,
+         .g = 0.0,
+         .b = 0.0,
          .a = 1.0,
       }}},
    }) catch return error.RendererCreateError;
