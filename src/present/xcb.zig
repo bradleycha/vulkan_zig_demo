5bb8e81ca58dd79cbaeb42b1af2858bdd224ec81
Chase Bradley <bradleycha@proton.me>
2023-11-11 22:41:38 -0500

    Implement XCB compositor

--- src/present/xcb.zig
+++ src/present/xcb.zig
@@ -4,7 +4,7 @@ const c        = @import("cimports");
 
 pub const VULKAN_REQUIRED_EXTENSIONS = struct {
    pub const Instance = [_] [*:0] const u8 {
-
+      c.VK_KHR_XCB_SURFACE_EXTENSION_NAME,
    };
 
    pub const Device = [_] [*:0] const u8 {
@@ -13,15 +13,45 @@ pub const VULKAN_REQUIRED_EXTENSIONS = struct {
 };
 
 pub const Compositor = struct {
+   _x_connection  : * c.xcb_connection_t,
+   _x_screen      : * c.xcb_screen_t,
+
    pub fn connect(allocator : std.mem.Allocator) f_shared.Compositor.ConnectError!@This() {
       _ = allocator;
-      unreachable;
+
+      var x_errno : c_int = undefined;
+
+      var x_screen_index : c_int = undefined;
+      const x_connection = c.xcb_connect(null, &x_screen_index) orelse unreachable;
+      errdefer c.xcb_disconnect(x_connection);
+
+      x_errno = c.xcb_connection_has_error(x_connection);
+      if (x_errno < 0) {
+         return error.PlatformError;
+      }
+
+      var x_setup = c.xcb_get_setup(x_connection) orelse unreachable;
+
+      var x_screen_iter = c.xcb_setup_roots_iterator(x_setup);
+      var x_screen_iter_index : c_int = 0;
+      while (x_screen_iter_index < x_screen_index) {
+         c.xcb_screen_next(&x_screen_iter);
+         x_screen_iter_index += 1;
+      }
+
+      const x_screen = x_screen_iter.data;
+
+      return @This(){
+         ._x_connection = x_connection,
+         ._x_screen     = x_screen,
+      };
    }
 
    pub fn disconnect(self : @This(), allocator : std.mem.Allocator) void {
-      _ = self;
       _ = allocator;
-      unreachable;
+
+      c.xcb_disconnect(self._x_connection);
+      return;
    }
 
    pub fn createWindow(self : * @This(), allocator : std.mem.Allocator, create_info : * const f_shared.Window.CreateInfo) f_shared.Window.CreateError!Window {
@@ -29,10 +59,7 @@ pub const Compositor = struct {
    }
 
    pub fn vulkanGetPhysicalDevicePresentationSupport(self : * const @This(), vk_physical_device : c.VkPhysicalDevice, vk_queue_family_index : u32) c.VkBool32 {
-      _ = self;
-      _ = vk_physical_device;
-      _ = vk_queue_family_index;
-      unreachable;
+      return c.vkGetPhysicalDeviceXcbPresentationSupportKHR(vk_physical_device, vk_queue_family_index, self._x_connection, self._x_screen.*.root_visual);
    }
 };
 
