b0ebb7befa7808b50aa06fc37869a9341cc8ec69
Chase Bradley <bradleycha@proton.me>
2023-12-26 19:13:18 -0500

    Implement AssetLoader functions create, destroy, get, and getMut

--- src/graphics/asset_loader.zig
+++ src/graphics/asset_loader.zig
@@ -5,19 +5,21 @@ const vulkan   = @import("vulkan/index.zig");
 
 const AssetLoader = @This();
 
-load_items                 : std.ArrayList(LoadItem),
+load_items                 : std.ArrayListUnmanaged(LoadItem),
 vk_command_buffer_transfer : c.VkCommandBuffer,
 vk_fence_ready             : c.VkFence,
 allocation_transfer        : vulkan.MemoryHeap.Allocation,
+
+const NULL_ALLOCATION_OFFSET = std.math.maxInt(u32);
 
 pub const LoadItem = struct {
    status   : Status,
    variant  : Variant,
 
    pub const Status = enum {
+      destroyed,  // slot available for use by another item
       pending,    // currently being loaded
       ready,      // ready for use
-      destroyed,  // slot available for use by another item
    };
 
    pub const VariantTag = enum {
@@ -55,9 +57,51 @@ pub const CreateInfo = struct {
 };
 
 pub fn create(create_info : * const CreateInfo) CreateError!AssetLoader {
-   // TODO: Implement
-   _ = create_info;
-   unreachable;
+   const vk_device                  = create_info.vk_device;
+   const vk_command_pool_transfer   = create_info.vk_command_pool_transfer;
+
+   var vk_result : c.VkResult = undefined;
+
+   const vk_info_create_fence_ready = c.VkFenceCreateInfo{
+      .sType   = c.VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
+      .pNext   = null,
+      .flags   = 0x00000000,
+   };
+
+   var vk_fence_ready : c.VkFence = undefined;
+   vk_result = c.vkCreateFence(vk_device, &vk_info_create_fence_ready, null, &vk_fence_ready);
+   switch (vk_result) {
+      c.VK_SUCCESS                     => {},
+      c.VK_ERROR_OUT_OF_HOST_MEMORY    => return error.OutOfMemory,
+      c.VK_ERROR_OUT_OF_DEVICE_MEMORY  => return error.OutOfMemory,
+      else                             => unreachable,
+   }
+   errdefer c.vkDestroyFence(vk_device, vk_fence_ready, null);
+
+   const vk_info_allocate_command_buffer_transfer = c.VkCommandBufferAllocateInfo{
+      .sType               = c.VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+      .pNext               = null,
+      .commandPool         = vk_command_pool_transfer,
+      .level               = c.VK_COMMAND_BUFFER_LEVEL_PRIMARY,
+      .commandBufferCount  = 1,
+   };
+
+   var vk_command_buffer_transfer : c.VkCommandBuffer = undefined;
+   vk_result = c.vkAllocateCommandBuffers(vk_device, &vk_info_allocate_command_buffer_transfer, &vk_command_buffer_transfer);
+   switch (vk_result) {
+      c.VK_SUCCESS                     => {},
+      c.VK_ERROR_OUT_OF_HOST_MEMORY    => return error.OutOfMemory,
+      c.VK_ERROR_OUT_OF_DEVICE_MEMORY  => return error.OutOfMemory,
+      else                             => unreachable,     
+   }
+   errdefer c.vkFreeCommandBuffers(vk_device, vk_command_pool_transfer, 1, &vk_command_buffer_transfer);
+
+   return AssetLoader{
+      .load_items                   = .{},
+      .vk_fence_ready               = vk_fence_ready,
+      .vk_command_buffer_transfer   = vk_command_buffer_transfer,
+      .allocation_transfer          = .{.offset = NULL_ALLOCATION_OFFSET, .bytes = undefined},
+   };
 }
 
 pub const DestroyInfo = struct {
@@ -66,10 +110,99 @@ pub const DestroyInfo = struct {
 };
 
 pub fn destroy(self : * AssetLoader, allocator : std.mem.Allocator, destroy_info : * const DestroyInfo) void {
+   const vk_device                  = destroy_info.vk_device;
+   const vk_command_pool_transfer   = destroy_info.vk_command_pool_transfer;
+
+   _checkLeakedAssets(self);
+
+   c.vkDestroyFence(vk_device, self.vk_fence_ready, null);
+   c.vkFreeCommandBuffers(vk_device, vk_command_pool_transfer, 1, &self.vk_command_buffer_transfer);
+   self.load_items.deinit(allocator);
+
+   return;
+}
+
+fn _checkLeakedAssets(self : * const AssetLoader) void {
+   if (std.debug.runtime_safety == false) {
+      return;
+   }
+
+   var validation_failed = false;
+
+   if (self.load_items.items.len != 0) {
+      std.log.err("assets still loaded", .{});
+      validation_failed = true;
+   }
+
+   if (self.allocation_transfer.offset != NULL_ALLOCATION_OFFSET) {
+      std.log.err("transfer allocation unfreed, call poll() before cleanup", .{});
+      validation_failed = true;
+   }
+
+   if (validation_failed == true) {
+      @panic("resources leaked on loader destruction");
+   }
+
+   return;
+}
+
+pub fn get(self : * const AssetLoader, handle : Handle) * const LoadItem {
+   const load_item = _getAllowDestroyed(self, handle);
+   _checkLoadItem(load_item);
+
+   return load_item;
+}
+
+pub fn getMut(self : * AssetLoader, handle : Handle) * LoadItem {
+   const load_item = _getAllowDestroyedMut(self, handle);
+   _checkLoadItem(load_item);
+
+   return load_item;
+}
+
+fn _getAllowDestroyed(self : * const AssetLoader, handle : Handle) * const LoadItem {
+   _checkHandle(self, handle);
+   return &self.load_items.items[handle];
+}
+
+fn _getAllowDestroyedMut(self : * AssetLoader, handle : Handle) * LoadItem {
+   _checkHandle(self, handle);
+   return &self.load_items.items[handle];
+}
+
+fn _checkHandle(self : * const AssetLoader, handle : Handle) void {
+   if (std.debug.runtime_safety == false) {
+      return;
+   }
+
+   if (handle >= self.load_items.items.len) {
+      @panic("attempted to access invalid load item");
+   }
+
+   return;
+}
+
+fn _checkLoadItem(load_item : * const LoadItem) void {
+   if (std.debug.runtime_safety == false) {
+      return;
+   }
+
+   if (load_item.status == .destroyed) {
+      @panic("attempted to access destroyed load item");
+   }
+
+   return;
+}
+
+pub const PollInfo = struct {
+   // TODO: Implement
+};
+
+pub fn poll(self : * AssetLoader, handle : Handle, poll_info : * const PollInfo) void {
    // TODO: Implement
    _ = self;
-   _ = allocator;
-   _ = destroy_info;
+   _ = handle;
+   _ = poll_info;
    unreachable;
 }
 
@@ -153,32 +286,6 @@ pub fn unload(self : * AssetLoader, handles : [] const Handle, unload_info : * c
    _ = self;
    _ = handles;
    _ = unload_info;
-   unreachable;
-}
-
-pub fn get(self : * const AssetLoader, handle : Handle) * const LoadItem {
-   // TODO: Implement
-   _ = self;
-   _ = handle;
-   unreachable;
-}
-
-pub fn getMut(self : * AssetLoader, handle : Handle) * LoadItem {
-   // TODO: Implement
-   _ = self;
-   _ = handle;
-   unreachable;
-}
-
-pub const PollInfo = struct {
-   // TODO: Implement
-};
-
-pub fn poll(self : * AssetLoader, handle : Handle, poll_info : * const PollInfo) void {
-   // TODO: Implement
-   _ = self;
-   _ = handle;
-   _ = poll_info;
    unreachable;
 }
 
