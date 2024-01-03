7358c8c2c7008547451d73bed493798523273836
Chase Bradley <bradleycha@proton.me>
2024-01-03 13:12:14 -0500

    Starting work on targa image parser

--- res/textures/index.zig
+++ res/textures/index.zig
@@ -1,55 +1,25 @@
 const std      = @import("std");
 const graphics = @import("graphics");
 
-// TODO: Instead of directly including raw image data, use comptime to parse
-// some normal format like bmp, png, etc. into raw image data.
+pub const TILE    = _embedTarga("tile.tga");
+pub const GRASS   = _embedTarga("grass.tga");
+pub const ROCK    = _embedTarga("rock.tga");
 
-pub const TILE = graphics.ImageSource{
-   .data    = @embedFile("tile.raw"),
-   .format  = .rgba8888,
-   .width   = 32,
-   .height  = 32,
-};
+fn _embedTarga(comptime path : [] const u8) graphics.ImageSource {
+   const bytes = @embedFile(path);
+   
+   var stream = std.io.FixedBufferStream([] const u8){.buffer = bytes, .pos = 0};
+   var reader = stream.reader();
 
-pub const GRASS = graphics.ImageSource{
-   .data    = @embedFile("grass.raw"),
-   .format  = .rgba8888,
-   .width   = 32,
-   .height  = 32,
-};
+   const image_source = _parseTarga(reader) catch |err| {
+      @compileError(std.fmt.comptimePrint("failed to parse image \'{s}\': {s}", .{path, @errorName(err)}));
+   };
 
-pub const ROCK = graphics.ImageSource{
-   .data    = @embedFile("rock.raw"),
-   .format  = .rgba8888,
-   .width   = 32,
-   .height  = 32,
-};
-
-// Verifies the byte lengths for every image source in the file.  This may seem
-// annoying when compilation fails, but it will save you from mystery crashes
-// and memory corruption at runtime.
-comptime {
-   const decls = @typeInfo(@This()).Struct.decls;
-
-   for (decls) |decl| {
-      const value = @field(@This(), decl.name);
-
-      if (std.mem.eql(u8, @typeName(@TypeOf(value)), @typeName(graphics.ImageSource)) == false) {
-         continue;
-      }
-
-      _verifyTextureDataLength(decl.name, &value);
-   }
+   return image_source;
 }
 
-fn _verifyTextureDataLength(comptime identifier : [] const u8, comptime source : * const graphics.ImageSource) void {
-   const expected_bytes = source.width * source.height * source.format.bytesPerPixel();
-   const found_bytes   = source.data.len;
-
-   if (expected_bytes != found_bytes) {
-      @compileError(std.fmt.comptimePrint("expected {} bytes of data for image source \'{s}\', instead found {} bytes", .{expected_bytes, identifier, found_bytes}));
-   }
-
-   return;
+fn _parseTarga(comptime reader : std.io.FixedBufferStream([] const u8).Reader) anyerror!graphics.ImageSource {
+   _ = reader;
+   return error.NotImplemented;
 }
 
